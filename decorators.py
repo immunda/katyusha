52bc5db431c03c6e02e41f0a6d13bab3d6fa97f2
Phil Howell <phil@quae.co.uk>
2013-05-30 16:55:06 +0100

    Tie in validation decorator stub

--- decorators.py
+++ decorators.py
@@ -1,15 +1,30 @@
 # -*- coding: utf-8 -*-
 from flask import Response
 from functools import wraps
-
 from flask import request
+from codeclubworld.api.models import BearerToken, Consumer
+from mongoengine.queryset import DoesNotExist
+from base64 import b64decode
 
 
 def authenticate(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        print request.headers
-        if True:
-            return Response(status=401)
-        return f(*args, **kwargs)
+        authorization_header = request.headers.get('Authorization', None)
+        if authorization_header is not None:
+            split_header = authorization_header.split(' ')
+            if len(split_header) == 2 and split_header[0] == 'Bearer':
+                encoded_token = split_header[1]
+                try:
+                    token = b64decode(encoded_token)
+                except TypeError:
+                    pass
+                else:
+                    try:
+                        BearerToken.objects.get(token=token)
+                    except DoesNotExist:
+                        pass
+                    else:
+                        return f(*args, **kwargs)
+        return Response(status=401)
     return decorated_function
