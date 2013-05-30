6e1050b64d1f09d7c34422ca7794d7161b82a60d
Phil Howell <phil@quae.co.uk>
2013-05-30 11:23:08 +0100

    Shoring up model definitions

--- models.py
+++ models.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 from mongoengine import *
 from datetime import datetime
-from flask import url_for
+import random
+
+
+def _generate_hash():
+    return '%032x' % random.getrandbits(128)
 
 
 class Consumer(Document):
-    name = StringField()
-    description = StringField()
-    key = StringField()
-    secret = StringField()
+    name = StringField(max_length=30, required=True)
+    description = StringField(max_length=150)
+    key = StringField(default=_generate_hash, required=True)
+    secret = StringField(default=_generate_hash, required=True)
+    created_at = DateTimeField(default=datetime.now, required=True)
 
     def __unicode__(self):
         return self.name
 
 
 class BearerToken(Document):
-    consumer = ReferenceField(Consumer)
-    token = StringField()
-    expires = DateTimeField()
+    consumer = ReferenceField(Consumer, required=True)
+    token = StringField(default=_generate_hash, required=True)
+    expires_at = DateTimeField()
+    created_at = DateTimeField(default=datetime.now, required=True)
 
     def __unicode__(self):
         return self.token
