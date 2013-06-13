26d775a354a3086582ed3336276d621bfaa7025a
Phil Howell <phil@quae.co.uk>
2013-06-13 10:59:46 +0100

    Tinkering with CORS header stuff

--- views.py
+++ views.py
@@ -38,7 +38,7 @@ class ApiView(MethodView):
     def unsupported_media_type(self):
         return Response(status=415)
 
-    @crossdomain(origin='*', headers=['*'])
+    @crossdomain(origin='*', headers=['accept', 'origin', 'authorization', 'content-type'])
     def options(self, **kwargs):
         return super(ApiView, self).options()
 
@@ -48,7 +48,7 @@ class TokenView(ApiView):
     Exchange consumer key & secret for bearer token
     """
 
-    @crossdomain(origin='*', headers=['*'])
+    @crossdomain(origin='*', headers=['accept', 'origin', 'authorization', 'content-type'])
     def post(self):
         grant_type = request.form.get('grant_type', '')
 
